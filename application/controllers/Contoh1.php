8c835b634cf402fa901324596a96dce01788715d
OktavianaPutriSuryaningsih09 <putrioktaviana43@gmail.com>
2023-03-30 08:58:46 +0700

    menambhakan contoh1 pustaka-booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Oktaviana Putri Suryaningsih,
+        Saya tingaal didaerah Depok,
+        Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}

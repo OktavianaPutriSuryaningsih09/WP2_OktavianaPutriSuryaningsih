b3c4027b317e88f3cf54b00e969fba8cf13481cd
OktavianaPutriSuryaningsih09 <putrioktaviana43@gmail.com>
2023-04-26 19:08:20 +0700

    menambahkan tugas pertemuan 7

---
+++ application/controllers/D-Lemas.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <tittle><<h2>D-Lemas<h2></tittle>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <href="http://localhost:8000/pustaka-booking/controllers/css/stylebuku.css">
+</head>
+   <form method="post" action="output_biodata.php"?>
+    <table border="1"align="center"width="500px"cellspacing="0">
+        <tr height="500px"
+         <tittle> <center><h2>Form Biodata<h2></center></tittle>
+    <td height="200px" valign="top">
+Nama            :<br><input type="textbox" id="nama" name="nama"><br>
+Tempat Lahir    :<br><input type="textbox" id="Tempatlahir" name="tempatlahir"><br>
+Tanggal Lahir   :<br><input type="date" id="tanggallahir" name="tanggallahir"><br>
+Alamat          :<br><input type="60" id="alamat" name="alamat"></textarea><br>
+Jenis Kelamain  :<br>
+                <select name="sex">
+                <option value="laki-laki">Laki-laki</Laki-laki><br> </option>
+                <option value="perempuan">Perempuan<br> </option>
+                <select><br>
+Agama           :<br>
+                <input type="radio"id="agama" name="Islam" value="Islam">Islam <br>
+                <input type="radio"id="agama" name="Kristen" value="Kristen">Kristen <br>
+                <input type="radio"id="agama" name="Katholik" value="Katholik">Katholik <br>
+                <input type="radio"id="agama" name="Budha" value="Budha">Budha <br>
+                <input type="radio"id="agama" name="Hindu" value="Hindu">Hindu <br>
+                <input type="radio"id="agama" name="Protestan" value="Protestan">Protestan <br>
+                <input type="radio"id="agama" name="Konghucu" value="Konghucu">Konghucu <br>
+<input type="submit" name="submit" value="simpan"> <input type="reset" name="reset" value="cancel">
+</td>
+</tr>
+</table>
+</body>
+</html>
---
+++ application/controllers/output_biodata.php
@@ -0,0 +1,13 @@
+<?php
+echo "Nama Saya :".$_POST['nama'];
+echo "<br />"
+echo "Tempat Lahir Saya :".$_POST['tempatlahir'];
+echo "<br />"
+echo "Tanggal Lahir Saya :".$_POST['tanggallahir'];
+echo "<br />"
+echo "Alamat Saya :".$_POST['alamat'];
+echo "<br />"
+echo "Jenis Kelamin Saya :".$_POST['sex'];
+echo "<br />"
+echo "Agama Saya :".$_POST['agama'];
+echo "<br />"
